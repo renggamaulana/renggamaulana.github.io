afca27f227e32edfd8f6d59c9500e39e38588017
Rengga Maulana <renggaa21@gmail.com>
2021-08-28 19:32:17 +0700

    mengembalikan ke vanilla html

--- src/App.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-  import Index from "./Route/Index.svelte";
-</script>
-
-<Index />
--- src/Pages/Blog.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import Navbar from "../components/Navbar.svelte";
-</script>
-
-<Navbar />
-<div class="container">
-  <h1>My Blog</h1>
-</div>
